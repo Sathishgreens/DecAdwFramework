150f43d958aadbacc192d7737be94bb04ff20383
Sathishgreens <sathishgreenstek@gmail.com>
2024-01-26 11:31:00 +0530

    jenkins

--- src/test/java/org/testngpractice/Sample.java
+++ src/test/java/org/testngpractice/Sample.java
@@ -9,36 +9,29 @@ import java.util.List;
 import org.base.BaseClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Sample extends BaseClass {
 
 	@Test(groups = "Sanity")
-	public void tc1() throws IOException {
-		browserLaunch("chrome");
-		maximizeWindow();
-		loadUrl("https://www.facebook.com/");
-
-		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-
-		System.out.println("Total Link : " + allLinks.size());
-		int brokenLinks = 0;
-		for (WebElement linkElement : allLinks) {
-			String link = linkElement.getAttribute("href");
-			try {
-				URL url = new URL(link);
-				URLConnection openConnection = url.openConnection();
-				HttpURLConnection connection = (HttpURLConnection) openConnection;
-				int responseCode = connection.getResponseCode();
-				if (responseCode != 200) {
-					brokenLinks++;
-				}
-			} catch (Exception e) {
-
-			}
-		}
-
-		System.out.println("Broken Links : " + brokenLinks);
+	public void tc1() {
+		System.out.println("@Test");
 	}
+	
+	@BeforeClass
+	public void befClass() {
+		System.out.println("@BeforeClass");
+	}
+	
+	@AfterClass
+	public void aftClass() {
+		System.out.println("@AfterClass");
+	}
+	
+	
+	
 }
